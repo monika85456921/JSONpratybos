3f20ea0cbba4ce4d3e2b645d4c3b106ae01e3236
Monika <okonecnikovaite.monika@gmail.com>
2023-12-10 23:43:34 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,256 @@
+const result = [
+    {
+      "adult": true,
+      "backdrop_path": "/backdrop1.jpg",
+      "genre_ids": [234, 567, 890],
+      "id": 67890,
+      "original_language": "fr",
+      "original_title": "French Film",
+      "overview": "A captivating French movie.",
+      "popularity": 6.7,
+      "poster_path": "/poster1.jpg",
+      "release_day": "2023-01-15",
+      "title": "Captivating French Film",
+      "video": true,
+      "vote_average": 7.2,
+      "vote_count": 800
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/backdrop2.jpg",
+      "genre_ids": [345, 678, 901],
+      "id": 78901,
+      "original_language": "es",
+      "original_title": "Spanish Adventure",
+      "overview": "An exciting Spanish adventure unfolds.",
+      "popularity": 8.2,
+      "poster_path": "/poster2.jpg",
+      "release_day": "2023-04-22",
+      "title": "Exciting Spanish Adventure",
+      "video": false,
+      "vote_average": 9.1,
+      "vote_count": 1200
+    },
+    {
+        "adult": false,
+        "backdrop_path": "/backdrop3.jpg",
+        "genre_ids": [456, 789, 123],
+        "id": 90123,
+        "original_language": "de",
+        "original_title": "German Mystery",
+        "overview": "A mysterious German film that keeps you on the edge of your seat.",
+        "popularity": 7.5,
+        "poster_path": "/poster3.jpg",
+        "release_day": "2023-07-08",
+        "title": "Intriguing German Mystery",
+        "video": true,
+        "vote_average": 8.0,
+        "vote_count": 950
+      },
+      {
+        "adult": true,
+        "backdrop_path": "/backdrop4.jpg",
+        "genre_ids": [567, 890, 234],
+        "id": 23456,
+        "original_language": "it",
+        "original_title": "Italian Drama",
+        "overview": "A gripping Italian drama that explores the complexities of human relationships.",
+        "popularity": 6.9,
+        "poster_path": "/poster4.jpg",
+        "release_day": "2023-09-30",
+        "title": "Compelling Italian Drama",
+        "video": false,
+        "vote_average": 7.8,
+        "vote_count": 1100
+      },
+      {
+        "adult": true,
+        "backdrop_path": "/backdrop5.jpg",
+        "genre_ids": [789, 123, 456],
+        "id": 34567,
+        "original_language": "ja",
+        "original_title": "Japanese Fantasy",
+        "overview": "Immerse yourself in the enchanting world of Japanese fantasy.",
+        "popularity": 8.4,
+        "poster_path": "/poster5.jpg",
+        "release_day": "2023-11-18",
+        "title": "Enchanting Japanese Fantasy",
+        "video": true,
+        "vote_average": 8.9,
+        "vote_count": 1500
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/backdrop6.jpg",
+        "genre_ids": [890, 234, 567],
+        "id": 45678,
+        "original_language": "ko",
+        "original_title": "Korean Thriller",
+        "overview": "Experience the suspense and thrills of a gripping Korean thriller.",
+        "popularity": 7.1,
+        "poster_path": "/poster6.jpg",
+        "release_day": "2023-05-12",
+        "title": "Suspenseful Korean Thriller",
+        "video": false,
+        "vote_average": 7.5,
+        "vote_count": 850
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/backdrop7.jpg",
+        "genre_ids": [123, 456, 789],
+        "id": 56789,
+        "original_language": "pt",
+        "original_title": "Portuguese Comedy",
+        "overview": "Laugh out loud with this hilarious Portuguese comedy.",
+        "popularity": 6.2,
+        "poster_path": "/poster7.jpg",
+        "release_day": "2023-03-25",
+        "title": "Hilarious Portuguese Comedy",
+        "video": true,
+        "vote_average": 7.0,
+        "vote_count": 920
+      },
+      {
+        "adult": true,
+        "backdrop_path": "/backdrop8.jpg",
+        "genre_ids": [234, 567, 890],
+        "id": 67890,
+        "original_language": "ru",
+        "original_title": "Russian Romance",
+        "overview": "Experience the depth of emotions in this heartwarming Russian romance.",
+        "popularity": 7.8,
+        "poster_path": "/poster8.jpg",
+        "release_day": "2023-08-10",
+        "title": "Heartwarming Russian Romance",
+        "video": false,
+        "vote_average": 8.3,
+        "vote_count": 1100
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/backdrop9.jpg",
+        "genre_ids": [567, 890, 123],
+        "id": 78901,
+        "original_language": "ar",
+        "original_title": "Arabic Action",
+        "overview": "Get ready for high-octane action in this thrilling Arabic film.",
+        "popularity": 8.1,
+        "poster_path": "/poster9.jpg",
+        "release_day": "2023-06-05",
+        "title": "Thrilling Arabic Action",
+        "video": true,
+        "vote_average": 8.6,
+        "vote_count": 1300
+      },
+      {
+        "adult": true,
+        "backdrop_path": "/backdrop10.jpg",
+        "genre_ids": [234, 567, 890],
+        "id": 90123,
+        "original_language": "zh",
+        "original_title": "Chinese Fantasy",
+        "overview": "Embark on a magical journey with this captivating Chinese fantasy.",
+        "popularity": 7.3,
+        "poster_path": "/poster10.jpg",
+        "release_day": "2023-09-15",
+        "title": "Captivating Chinese Fantasy",
+        "video": false,
+        "vote_average": 7.9,
+        "vote_count": 950
+      },
+      {
+        "adult": false,
+        "backdrop_path": "/backdrop11.jpg",
+        "genre_ids": [890, 123, 456],
+        "id": 12345,
+        "original_language": "fr",
+        "original_title": "French Romance",
+        "overview": "Experience the passion of a beautiful French romance.",
+        "popularity": 7.5,
+        "poster_path": "/poster11.jpg",
+        "release_day": "2023-11-01",
+        "title": "Passionate French Romance",
+        "video": true,
+        "vote_average": 8.2,
+        "vote_count": 1100
+      },
+      {
+        "adult": true,
+        "backdrop_path": "/backdrop12.jpg",
+        "genre_ids": [567, 890, 234],
+        "id": 2345644,
+        "original_language": "es",
+        "original_title": "Spanish Comedy",
+        "overview": "Laugh your heart out with this hilarious Spanish comedy.",
+        "popularity": 6.9,
+        "poster_path": "/poster12.jpg",
+        "release_day": "2023-07-20",
+        "title": "Hilarious Spanish Comedy",
+        "video": false,
+        "vote_average": 7.8,
+        "vote_count": 950
+      }
+  ]
+  
+///1 patikrinti ar yra favorites
+const arYraFavorites = () => {
+  let infoApieFav = localStorage.getItem("favorites");
+  return infoApieFav === null ? []: JSON.parse(infoApieFav);
+}
+const patikrintas =  arYraFavorites();
+console.log(patikrintas);
+
+//2 //3
+patikrintas.push(["Hilarious Spanish Comedy","Passionate French Romance","Captivating Chinese Fantasy"]);
+console.log(patikrintas);
+
+localStorage.setItem("favorites", JSON.stringify(patikrintas));
+
+const arTeisingaiIrasytiDuom = JSON.parse(localStorage.getItem("favorites"));
+
+console.log(arTeisingaiIrasytiDuom);
+
+//4
+
+
+// function rastiFilma = (id) =>{
+//   for(let i = 0; i < result.length; i++){
+//     if (result[i].id === id){
+
+//     }
+//   }
+// }
+
+function rastiFilma(id){
+  for(let i = 0; i < result.length; i++){
+    if(result[i].id === id)
+    return result[i].overview
+  }
+}
+const apieFilma = rastiFilma(2345644);
+console.log(apieFilma);
+
+//5
+
+// localStorage.setItem("id", 2345644);
+// localStorage.setItem("title", "Hilarious Spanish Comedy" );
+
+const pasirinktas = result[1];
+
+const filmoTitle = pasirinktas.title;
+const filmoId = pasirinktas.id; 
+
+console.log(filmoTitle);
+
+localStorage.setItem("filmoId", filmoId);
+localStorage.setItem("filmoTitle", filmoTitle);
+
+//6
+function megstamiIsLs() {
+    return JSON.parse(localStorage.getItem("favorites"));
+}
+const megstami = megstamiIsLs();
+console.log(megstami);
+
+//7
